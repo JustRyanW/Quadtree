51e4ae9402064fd7cf44b45a39323d2787b2357f
Ryan <xryanjw@gmail.com>
2020-05-05 20:51:14 +0100

    Chunk loading / queueing

--- Assets/Scripts/WorldGen3D.cs
+++ Assets/Scripts/WorldGen3D.cs
@@ -3,42 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldGen3D : MonoBehaviour {
-    public Octree octree;
+    public Chunk chunk;
+    public Queue<Vector3Int> chunksToLoad = new Queue<Vector3Int>();
+
+    [Header("Settings")]
+    public int chunkSize = 4;
+    public int lod = 0;
+    public int renderDistance = 1;
+
+    [Header("Debug")]
+    public bool drawChunk;
+    public int drawSubdivisions;
+    public bool drawSurface;
+    public bool drawVoxels;
+
+    private int size;
+    private Vector3Int chunkPos;
 
     private void Start() {
         World3D.voxels.Clear();
+        // new Chunk(new Vector3Int(0, 0, 0), chunkSize, lod);
+    }
 
-        int size = (int)Mathf.Pow(2, 6);
-        for (int x = 0; x <= size; x++) {
-            for (int y = 0; y <= size; y++) {
-                for (int z = 0; z <= size; z++) {
-                    Vector3Int position = new Vector3Int(x, y, z);
-                    World3D.voxels[position] = new Voxel(position);
+    private void Update() {
+        Vector3Int newChunkPos = Vector3Int.FloorToInt(Camera.main.transform.position / (1 << chunkSize));
+
+        if (newChunkPos != chunkPos) {
+            chunkPos = newChunkPos;
+
+            for (int x = chunkPos.x - renderDistance; x < chunkPos.x + renderDistance; x++) {
+                for (int y = chunkPos.y - renderDistance; y < chunkPos.y + renderDistance; y++) {
+                    for (int z = chunkPos.z - renderDistance; z < chunkPos.z + renderDistance; z++) {
+                        Vector3Int pos = new Vector3Int(x, y, z);
+                        if (!World3D.chunks.ContainsKey(pos) && !chunksToLoad.Contains(pos)) {
+                            chunksToLoad.Enqueue(pos);
+                        }
+                    }
                 }
             }
         }
 
-        octree = new Octree(new Vector3Int(0,0, 0), size);
-        octree.Subdivide(6);
-
-        Debug.Log(World3D.surface.Count);
+        for (int i = 0; i < 30; i++)
+        {
+            if (chunksToLoad.Count > 0) {
+                new Chunk(chunksToLoad.Dequeue(), chunkSize, lod);
+            }
+        }
     }
 
     private void OnDrawGizmos() {
-        if (octree != null) {
-            octree.DrawWire();
+        if (drawChunk) {
+            foreach(KeyValuePair<Vector3Int, Chunk> chunk in World3D.chunks) {
+                chunk.Value.octree.DrawWire(drawSubdivisions);     
+                if (drawSurface) {
+                    foreach (Octree surface in chunk.Value.surface) {
+                        Gizmos.DrawWireCube(surface.position + new Vector3(0.5f, 0.5f, 0.5f) * surface.size, Vector3.one * surface.size);
+                    }
+                }
+            }
         }
 
-        // foreach (Octree surface in World3D.surface) {
-        //     Gizmos.DrawWireCube(surface.position + new Vector3(0.5f, 0.5f, 0.5f) * surface.size, Vector3.one * surface.size);
-        // }
+        if (drawVoxels) {
+            foreach(KeyValuePair<Vector3Int, Voxel> voxel in World3D.voxels) {
+                Gizmos.color = Color.Lerp(Color.black, Color.white, voxel.Value.value);
+                Gizmos.DrawCube(voxel.Key, Vector3.one * 0.2f);
+            }
+        }
+    }
+
+    private void OnValidate() {
+        chunkSize = Mathf.Clamp(chunkSize, 1, 4);
+        size = 1 << chunkSize;
+        lod = Mathf.Clamp(lod, 0, chunkSize);
+        drawSubdivisions = Mathf.Clamp(drawSubdivisions, 0, chunkSize - lod);
+        if (World3D.voxels.Count > 10000 && drawVoxels) {
+            drawVoxels = false;
+            Debug.Log("Too many voxel to draw!");
+        }
     }
 }
 
 public static class World3D {
     public static Dictionary<Vector3Int, Voxel> voxels = new Dictionary<Vector3Int, Voxel>();
-    public static Dictionary<Vector3Int, Octree> chunks = new Dictionary<Vector3Int, Octree>();
-    public static List<Octree> surface = new List<Octree>();
+    public static Dictionary<Vector3Int, Chunk> chunks = new Dictionary<Vector3Int, Chunk>();
     public static Noise noise = new Noise();
 
     public static Vector3Int GetCorner(int index) {
@@ -50,39 +97,85 @@ public static class World3D {
     }
 }
 
+public class Chunk {
+    public Vector3Int chunkPosition;
+    public Vector3Int position;
+    public int size;
+    public int maxDepth;
+    public int lod;
+    public Octree octree;
+    public List<Octree> surface = new List<Octree>();
+
+    public Chunk(Vector3Int chunkPosition, int chunkSize, int lod) {
+        World3D.chunks[chunkPosition] = this;
+        this.chunkPosition = chunkPosition;
+        size = 1 << chunkSize;
+        this.position = chunkPosition * size;
+        this.lod = lod;
+        maxDepth = chunkSize - lod;
+
+        GenerateTerrainData();
+
+        octree = new Octree(position, size, this);
+        octree.Subdivide(maxDepth);
+    }
+
+    public void GenerateTerrainData() {
+        for (int x = position.x; x <= position.x + size; x += 1 << lod) {
+            for (int y = position.y; y <= position.y + size; y += 1 << lod) {
+                for (int z = position.z; z <= position.z + size; z += 1 << lod) {
+                    Vector3Int pos = new Vector3Int(x, y, z);
+                    if (!World3D.voxels.ContainsKey(pos)) {
+                        new Voxel(pos);
+                    }
+                }
+            }
+        }
+    }
+}
+
 public class Octree {
     public Vector3Int position;
     public int size;
+    public Chunk chunk;
 
     public Voxel[] corners = new Voxel[8];
     public Octree[] octrees;
 
-    public Octree(Vector3Int position, int size) {
+    public Octree(Vector3Int position, int size, Chunk chunk) {
         this.position = position;
         this.size = size;
+        this.chunk = chunk;
+
         for (int i = 0; i < 8; i++) {
-            corners[i] = World3D.voxels[position + World3D.GetCorner(i)];
+            Vector3Int pos = position + World3D.GetCorner(i) * size;
+            if (!World3D.voxels.ContainsKey(pos)) {
+                new Voxel(pos);
+                Debug.Log("No voxel data!");
+            }
+            corners[i] = World3D.voxels[pos];
         }
     }
 
     public void Subdivide(int depth = 1) {
-        if ((size / 2f) % 1 == 0) {
-            int halfSize = size / 2;
-            octrees = new Octree[8];   
+        if (depth > 0) {
             depth--;
-            for (int i = 0; i < 8; i++) {
-                octrees[i] = new Octree(position + World3D.GetCorner(i) * halfSize, halfSize);
-
-                if (octrees[i].HasSurface()) {
-                    if (depth > 0) {
+            if ((size / 2f) % 1 == 0) {
+                int halfSize = size / 2;
+                octrees = new Octree[8];
+                for (int i = 0; i < 8; i++) {
+                    octrees[i] = new Octree(position + World3D.GetCorner(i) * halfSize, halfSize, chunk);
+                    if (octrees[i].HasSurface()) {
                         octrees[i].Subdivide(depth);
-                    } else {
-                        World3D.surface.Add(octrees[i]);
                     }
                 }
+            } else {
+                Debug.Log("Cannot subdivide further!");
             }
         } else {
-            Debug.Log("Cannot subdivide further.");
+            if (HasSurface()) {
+                chunk.surface.Add(this);
+            }
         }
     }
 
@@ -104,11 +197,12 @@ public class Octree {
         return hasSurface;
     }
 
-    public void DrawWire() {
+    public void DrawWire(int depth = 0) {
         Gizmos.DrawWireCube(position + new Vector3(0.5f, 0.5f, 0.5f) * size, Vector3.one * size);
-        if (octrees != null) {
+        depth--;
+        if (depth > 0  && octrees != null) {
             for (int i = 0; i < 8; i++) {
-                octrees[i].DrawWire();
+                octrees[i].DrawWire(depth);
             }
         }
     }
@@ -118,6 +212,7 @@ public class Voxel {
     public float value;
 
     public Voxel(Vector3Int position) {
-        value = World3D.noise.Evaluate((Vector3)position * 0.03f);
+        World3D.voxels[position] = this;
+        value = World3D.noise.Evaluate((Vector3)position * 0.005f);
     }
 }
