a3e4570afddca5fdf3d7fdba8c2481e05ba95dde
Ryan <xryanjw@gmail.com>
2020-05-05 07:55:25 +0100

    Auto Subdivide

--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -6,8 +6,9 @@ public class WorldGen : MonoBehaviour {
     Quadtree quad;
 
     private void Start() {
-        quad = new Quadtree(new Vector2Int(0,0), 8);
-        quad.Subdivide();
+        World.pixels.Clear();
+        quad = new Quadtree(new Vector2Int(0,0), (int)Mathf.Pow(2, 5));
+        quad.Subdivide(5);
 
         Debug.Log(World.pixels.Count);
     }
@@ -20,7 +21,7 @@ public class WorldGen : MonoBehaviour {
         foreach (KeyValuePair<Vector2Int, Pixel> pixel in World.pixels)
         {
             Gizmos.color = Color.Lerp(Color.black, Color.white, pixel.Value.value);
-            Gizmos.DrawCube((Vector2)pixel.Key, Vector3.one * 0.2f);
+            Gizmos.DrawCube((Vector2)pixel.Key, Vector3.one * 0.4f);
         }
     }
 }
@@ -53,20 +54,39 @@ public class Quadtree {
         for (int i = 0; i < 4; i++) {
             Vector2Int pos = position + GetCorner(i) * size;
             if (!World.pixels.ContainsKey(pos)) {
-                World.pixels[pos] = new Pixel(Random.Range(0f, 1f));
+                World.pixels[pos] = new Pixel(Mathf.PerlinNoise(pos.x * 0.05f, pos.y * 0.05f));
             }
             corners[i] = World.pixels[pos];
         }
     }
 
-    public void Subdivide() {
-        if (size / 2 % 1 == 0) {
+    public void Subdivide(int depth = 1) {
+        if ((size / 2f) % 1 == 0) {
             int halfSize = size / 2;
-            quadtrees = new Quadtree[4];
+            quadtrees = new Quadtree[4];   
+            depth--;
             for (int i = 0; i < 4; i++) {
                 quadtrees[i] = new Quadtree(position + GetCorner(i) * halfSize, halfSize);
+                if (depth > 0) {
+                    if (quadtrees[i].IsSurface()) {
+                        quadtrees[i].Subdivide(depth);
+                    }
+                }
+            }
+        } else {
+            Debug.Log("Cannot subdivide further.");
+        }
+    }
+
+    public bool IsSurface() {
+        bool inTerrain = corners[0].value > 0.5f;
+        bool isSurface = false;
+        for (int i = 1; i < 4; i++) {
+            if ((corners[i].value > 0.5f) != inTerrain) {
+               isSurface = true;
             }
         }
+        return isSurface;
     }
 
     public static Vector2Int GetCorner(int index) {
