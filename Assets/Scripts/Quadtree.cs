9a8dbf5ec0763e8e7da1bd87d084da46494421d1
Ryan <xryanjw@gmail.com>
2020-05-05 06:16:58 +0100

    Quadtree start

---
+++ Assets/Scripts/Quadtree.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class World {
+    public static Dictionary<Vector2Int, Pixel> pixels = new Dictionary<Vector2Int, Pixel>();
+}
+
+public class Pixel {
+    Vector2Int position;
+    float value;
+}
+
+public class Quadtree {
+    public Pixel[] corners;
+}
